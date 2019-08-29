a4349721dba158a31abf3551d67e62a1f0d6e917
chengqiu <chengqiu@chengqiu-pc>
2019-08-29 14:30:58 -0400

    first try

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,56 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		int count = scan.nextInt();
 		
+		String[] fnames = new String [count];
+		String[] lnames = new String [count];
+		String[] fletter = new String [count];
+		double[] totalp = new double [count];
+		
+		
+		//first loop
+		for (int i = 0; i< fnames.length; i++) {
+			fnames [i] = scan.next();
+			lnames [i] = scan.next();
+			int count2 = scan.nextInt();
+			//declare parameter
+			String[] item = new String [count2];
+			int[] quantity = new int [count2];
+			double[] price = new double [count2];
+			
+			///second loop
+			for (int i2 = 0; i2< item.length; i2++) {
+				quantity [i2] = scan.nextInt();
+				item [i2] = scan.next();
+				price[i2] = scan.nextDouble();
+				
+				double total = 0;
+				/// loop to calculate total price for each person
+				for (int i3 = 0;i3 < item.length;i3++) {
+				   for(int j = 0; j < item.length;j++) {
+				    if (i3==j){
+					   total += quantity[i3]*price[j];
+				    }
+				   
+				   }
+				}
+			totalp [i] = total; 					
+			}
+			
+		}
+		scan.close();
+		for (int i =0; i < fnames.length;i++){
+			 fletter[i] = Character.toString(fnames[i].charAt(0));
+			 
+		}
+	
+		for (int i =0; i< fnames.length;i++){
+			System.out.println(fletter [i] +". "+ lnames [i] + ": " + totalp[i]);
+		
+		}
 	}
+	
+	
+	
 }
